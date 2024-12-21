549a88f40c59f2f5ae6bd0a896036f7845f081cf
golatif <136717263+leanwrldd@users.noreply.github.com>
2024-12-21 10:32:22 +0300

    v4
    added api to verify user and user state for storing user password

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,16 +1,33 @@
 'use client'
+
 import PasswordInput from '/app/components/passwordinput.js';
 import GithubIcon from '/app/components/github.js';
 import GoBack from '/app/components/goback.js';
 import Button from '/app/components/buttonv1.js';
+import { useState } from 'react';
 
 export default function Admin() {
+  const [password, setPassword] = useState('');
   const handlePasswordChange = (newPassword) => {
+    setPassword(newPassword);
     console.log('Password changed:', newPassword);
+
   };
+
   const HandleClick = () => {
     console.log('Clicked');
   };
+  const HandleAuthentication = async (event) => {
+    event.preventDefault();
+    const response = await fetch('/api/verifyuser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password }),
+    });
+  };
+
   return (
     <>
       <div className='flex flex-col justify-center items-center custom-grid min-h-screen'>
--- pages/api/verifyuser.js
+++ pages/api/verifyuser.js
@@ -1,5 +1,22 @@
-export default async function Handler({req, res}){
-    if (req.method !== "POST"){
-        return res.status(405).json({error:"Method Not Allowed : ${req.method}"});
-    }
+import fs from 'fs';
+import path from 'path';
+
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: `Method Not Allowed: ${req.method}` });
+  }
+
+  const { password } = req.body;
+
+  const filePath = path.join(process.cwd(), 'config.json');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const { adminPassword } = JSON.parse(fileContents);
+
+  if (password === adminPassword){
+    return res.status(200).JSON({message:"Password verified."});
+  }
+  else {
+    return res.status(401).json({error:"Invalid password."});
+  }
 }
