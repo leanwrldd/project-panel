c491afbc2387cb8c3fd2ce16887f337080071481
golatif <136717263+leanwrldd@users.noreply.github.com>
2024-12-21 19:49:16 +0300

    v7
    finished the selection box

--- app/components/selectionbox.js
+++ app/components/selectionbox.js
@@ -1,21 +1,37 @@
 'use client'
-import { useState } from "react";
+import React, { useState } from 'react';
 
-export default function SelectionBox({ data }) {
-    const [option, setOption] = useState('');
+export default function SelectionBox({ data, onOptionChange}) {
+    const [option, setOption] = useState(data[0] || '');
 
+    const handleOptionChange = (item) => {
+        setOption(item);
+        if (onOptionChange) {
+            onOptionChange(item);
+        }
+    };
     return (
-        <div className="flex flex-col items-start p-2 text-black bg-white w-1/4 rounded-xl">
+        <div className="flex flex-col items-start p-2 text-black bg-white w-1/5 rounded-xl">
             <div className="text-left lato-bold m-3 text-2xl">
-                <h1>Select option</h1>
+                <h1>Select class</h1>
             </div>
-            <div>
-                
-            {data.map((item, index) => (
-                <div key={index} className="m-5">
-                    <label className="text-xl">{item}</label>
-                </div>
-            ))}
+            <div className="w-full max-h-[600px] overflow-y-auto">
+                {data.map((item, index) => (
+                    <div
+                        key={index}
+                        className="m-5 font-medium h-14 hover:bg-zinc-100 flex items-center px-3 rounded-lg has-[:checked]:text-blue-500 has-[:checked]:bg-blue-50 has-[:checked]:ring-blue-300 has-[:checked]:ring-1 select-none"
+                        onClick={() => handleOptionChange(item)}
+                    >
+                        <label className="text-xl"> {item}</label>
+                        <input
+                            type="radio"
+                            name="status"
+                            id={index}
+                            checked={option === item}
+                            className="hidden"
+                        />
+                    </div>
+                ))}
             </div>
         </div>
     );
--- app/page.js
+++ app/page.js
@@ -1,12 +1,22 @@
+'use client';
 import GoBackRedirect from '/app/components/goback-Redirect.js';
 import SelectionBox from '/app/components/selectionbox.js';
 
+import { useState } from 'react';
+
 export default function Home() {
-  const data = ["hey1", "hey2", "hey3"];
+  const [selectedClass, setSelectedClass] = useState(null);
+
+  const handleChange = (item) => {
+    setSelectedClass(item);
+  };
+
+  const data = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7', 'Class 8', 'Class 9', 'Class 10', 'Class 11'];
+
   return (
     <>
       <div className='flex justify-center items-center h-screen'>
-      <SelectionBox data={data}/>
+        <SelectionBox data={data} onOptionChange={handleChange} />
       </div>
     </>
   );
