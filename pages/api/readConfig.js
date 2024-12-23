0c48d42ccae54f5484e95b0884fb2a55471a7c4e
golatif <136717263+leanwrldd@users.noreply.github.com>
2024-12-23 21:15:09 +0300

    v0.0.8.3

--- pages/api/readConfig.js
+++ pages/api/readConfig.js
@@ -6,21 +6,34 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: `Method Not Allowed: ${req.method}` });
   }
 
+  let cachedConfig = null;
   const { password } = req.body;
+  const { request } = req.body;
 
   try {
     const filePath = path.join(process.cwd(),'public','config.json');
     const fileContents = fs.readFileSync(filePath, 'utf8');
-    const config = JSON.parse(fileContents);
-    const adminPassword = config.auth.password;
-
-    if (password === adminPassword) {
+    cachedConfig = JSON.parse(fileContents);
+  }
+  catch (error){
+    console.error('Error reading config file:', error);
+    return res.status(500).json({ error: "Internal Server Error, Couldn't Read Config file." });
+  }
+  if (request==="password"){
+    if (password === cachedConfig.auth.password) {
       return res.status(200).json({ message: "Password verified." });
     } else {
       return res.status(401).json({ error: "Invalid password." });
     }
-  } catch (error) {
-    console.error('Error reading config file:', error);
-    return res.status(500).json({ error: "Internal Server Error" });
   }
+  else if (request==="config") {
+    return res.status(200).json(cachedConfig.options);
+  }
+  else {
+    console.error("Invalid Request, Request method not found. ${request}");
+    return res.status(400).json({ error: "Invalid request."})
+  }
+
+
+
 }
